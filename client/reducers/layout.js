aeb5ddbaa39ba61aaac51c98c3c68b71706c5edd
manny404 <manny@art404.com>
2016-03-02 02:42:59 -0500

    remove console.logs

--- client/reducers/layout.js
+++ client/reducers/layout.js
@@ -3,8 +3,6 @@ import * as types from '../constants/ActionTypes'
 export default function layout (state = {sideOpen: false}, action) {
   switch (action.type) {
     case types.TOGGLE_SIDEBAR:
-      console.log(action)
-      console.log(state)
       return {
         sideOpen: action.sideOpen
       }
